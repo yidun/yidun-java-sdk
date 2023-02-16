6efab48a7956cba89d669cbee2c2de1f43ab2fc0
zhaojipu <zhaojipu@corp.netease.com>
2023-02-16 14:01:51 +0800

    图片增加针对批量主动回调的解析支持

--- yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/image/v5/callback/request/ImageV5ActiveCallbackRequest.java
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/image/v5/callback/request/ImageV5ActiveCallbackRequest.java
@@ -1,7 +1,10 @@
 package com.netease.yidun.sdk.antispam.image.v5.callback.request;
 
+import com.google.gson.reflect.TypeToken;
 import com.netease.yidun.sdk.antispam.callback.ActiveCallbackRequest;
 import com.netease.yidun.sdk.antispam.image.v5.check.sync.response.ImageV5Result;
+
+import java.util.List;
 
 /**
  * 图片主动回调实体
@@ -14,4 +17,14 @@ public class ImageV5ActiveCallbackRequest extends ActiveCallbackRequest {
         return parseCallbackData(ImageV5Result.class);
     }
 
+    /**
+     * 回调的数据存在两种，一种是单条数据，一种多条数据
+     * 此方法会将单条数据和多条数据的格式统一为列表格式返回
+     *
+     * @return
+     */
+    public List<ImageV5Result> parseImageCallbackDataList() {
+        return parseCallbackData(ImageV5Result.class, new TypeToken<List<ImageV5Result>>() {
+        });
+    }
 }
