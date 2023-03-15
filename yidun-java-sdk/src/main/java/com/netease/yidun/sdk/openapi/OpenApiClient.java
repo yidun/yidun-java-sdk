ec700013c86326f7635afc0cec5951ff1d349ff6
祝力炀 <zhuliyang02@corp.netease.com>
2023-03-15 11:03:45 +0800

    OMSEC-71143 【接口文档】【SDK更新】内容安全机审标签查询接口提供接入文档与SDK https://overmind.hz.netease.com/177/requirement/issues/OMSEC-71143
    (cherry picked from commit fca13e2c44229b23a455b8cd4c3ec98d732f74cd)

--- yidun-java-sdk/src/main/java/com/netease/yidun/sdk/openapi/OpenApiClient.java
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/openapi/OpenApiClient.java
@@ -4,9 +4,11 @@
  * Copyright 2023 NetEase.com, Inc. All rights reserved.
  */
 
-package com.netease.yidun.sdk.antispam.label;
+package com.netease.yidun.sdk.openapi;
 
 import com.netease.yidun.sdk.antispam.AntispamClient;
+import com.netease.yidun.sdk.antispam.AntispamRequester;
+import com.netease.yidun.sdk.antispam.BaseClient;
 import com.netease.yidun.sdk.antispam.label.query.request.LabelQueryRequest;
 import com.netease.yidun.sdk.antispam.label.query.response.LabelQueryResponse;
 import com.netease.yidun.sdk.core.auth.OpenApiSignerImpl;
@@ -16,10 +18,8 @@ import com.netease.yidun.sdk.core.client.ClientProfile;
  * @author zhuliyang02
  * @version 2023-03-14
  */
-public class LabelQueryClient extends AntispamClient {
-
-
-    public LabelQueryClient(ClientProfile clientProfile) {
+public class OpenApiClient extends AntispamClient {
+    public OpenApiClient(ClientProfile clientProfile) {
         clientProfile.setSigner(OpenApiSignerImpl.INSTANCE);
         client = createClient(clientProfile);
     }
