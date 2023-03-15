00268bce79254db0cfd6de226a3987059ff47fc7
祝力炀 <zhuliyang02@corp.netease.com>
2023-03-15 10:01:36 +0800

    OMSEC-71143 【接口文档】【SDK更新】内容安全机审标签查询接口提供接入文档与SDK https://overmind.hz.netease.com/177/requirement/issues/OMSEC-71143
    (cherry picked from commit bc4f6090cd94e00ae77bdb0a928a8b4a2a00f218)

--- yidun-java-sdk-demo/src/main/java/com/netease/yidun/sdk/antispam/label/LabelQueryDemo.java
+++ yidun-java-sdk-demo/src/main/java/com/netease/yidun/sdk/antispam/label/LabelQueryDemo.java
@@ -10,6 +10,7 @@ import com.netease.yidun.sdk.antispam.AbstractDemo;
 import com.netease.yidun.sdk.antispam.AntispamRequester;
 import com.netease.yidun.sdk.antispam.enums.LabelBusinessTypeEnum;
 import com.netease.yidun.sdk.antispam.label.query.request.LabelQueryRequest;
+import com.netease.yidun.sdk.antispam.label.query.response.LabelQueryInfo;
 import com.netease.yidun.sdk.antispam.label.query.response.LabelQueryResponse;
 
 import java.util.ArrayList;
@@ -23,22 +24,27 @@ import java.util.List;
 public class LabelQueryDemo extends AbstractDemo {
 
     public static void  main(String[] args) {
-//        AntispamRequester antispamRequester = createAntispamRequester("SecretId", "SecretKey");
-        AntispamRequester antispamRequester = createAntispamRequester("YvdOsdj21k5Iod_jkmLimi_D0gA", "iiDzJiIaOkuG0yrU_uejZ-TXUno");
+        AntispamRequester antispamRequester = createAntispamRequester("SecretId", "SecretKey");
 
         LabelClient labelClient = new LabelClient(antispamRequester);
 
         LabelQueryRequest labelQueryRequest = new LabelQueryRequest();
-//        labelQueryRequest.setBusinessId("0a9753a437545cc599f7b5ea785c224d");
-//        labelQueryRequest.setClientId("9abfd9256dfb4691bfb86892828b2d2c4592072246258548904");
+        labelQueryRequest.setBusinessId("businessId");
+        labelQueryRequest.setClientId("clientId");
         labelQueryRequest.setMaxDepth(3);
 
         List<String> businessTypes = new ArrayList<>();
         businessTypes.add(LabelBusinessTypeEnum.TEXT.getCode());
 
         labelQueryRequest.setBusinessTypes(businessTypes);
+        try {
+            LabelQueryResponse labelQueryResponse = labelClient.queryLabel(labelQueryRequest);
+            if (labelQueryResponse.getCode() == 200 && labelQueryResponse.getData() != null) {
+                List<LabelQueryInfo> labelQueryInfos = labelQueryResponse.getData();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        LabelQueryResponse labelQueryResponse = labelClient.queryLabel(labelQueryRequest);
-        System.out.println();
     }
 }
