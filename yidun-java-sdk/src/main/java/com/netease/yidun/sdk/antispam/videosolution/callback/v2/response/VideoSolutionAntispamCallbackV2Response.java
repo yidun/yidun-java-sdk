faa3a8e28edad413bb48d395f58ae85c8135de57
liangtingchen <liangtingchen@corp.netease.com>
2023-04-23 15:39:15 +0800

    点播音视频回调增加截图张数
    (cherry picked from commit ec98175e389677477b8d7e12a254ac9e32f94687)

--- yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/videosolution/callback/v2/response/VideoSolutionAntispamCallbackV2Response.java
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/videosolution/callback/v2/response/VideoSolutionAntispamCallbackV2Response.java
@@ -57,6 +57,10 @@ public class VideoSolutionAntispamCallbackV2Response {
      */
     private Integer label;
     /**
+     * 截图数量
+     */
+    private Long picCount;
+    /**
      * 机器检测证据信息，接入机器检测时参考证据信息
      */
     private VideoSolutionCallbackEvidenceV2Response evidences;
