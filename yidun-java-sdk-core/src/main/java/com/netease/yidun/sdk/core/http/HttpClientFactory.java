f1f23b03cccd39678d920d3b7e793eda53747e1f
fengyunlong <fengyunlong@corp.netease.com>
2023-05-30 18:02:04 +0800

    OMSEC-77615 添加自定义user-agent
    添加自定义user-agent
    
    (cherry picked from commit cc79d34a8df658432aa12f3c035c5c4c454b013c)

--- yidun-java-sdk-core/src/main/java/com/netease/yidun/sdk/core/http/HttpClientFactory.java
+++ yidun-java-sdk-core/src/main/java/com/netease/yidun/sdk/core/http/HttpClientFactory.java
@@ -2,15 +2,17 @@
 
 package com.netease.yidun.sdk.core.http;
 
-import java.util.concurrent.TimeUnit;
-
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.core5.http.io.SocketConfig;
 import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.VersionInfo;
+
+import java.util.concurrent.TimeUnit;
 
 public class HttpClientFactory {
 
@@ -38,6 +40,30 @@ public class HttpClientFactory {
                 .setConnectionManager(connManager)
                 .setDefaultRequestConfig(requestConfig)
                 .useSystemProperties()
+                // 设置版本号在ua上，方便统计
+                .setUserAgent(getCustomizeSdkUserAgent())
                 .build();
     }
+
+    /**
+     *
+     * @return
+     */
+    private static String getCustomizeSdkUserAgent() {
+        return HttpHeaders.SDK_VERSION
+                .concat("/")
+                .concat(HttpHeaders.DEFAULT_SDK_VERSION)
+                .concat(";")
+                .concat(getDefaultUserAgent());
+    }
+
+    /**
+     * 获取默认的user-agent（和HttpClientBuilder包中设置的保持一致）
+     * @return 默认的user-agent
+     */
+    private static String getDefaultUserAgent() {
+        return VersionInfo.getSoftwareInfo("Apache-HttpClient",
+                "org.apache.hc.client5", HttpClientBuilder.class);
+    }
+
 }
