f77014be733bf8abe32d10e12aed7630ab000f20
zhangze03 <zhangze03@corp.netease.com>
2024-01-22 17:52:14 +0800

    OMSEC-97317 【后端】【分期需求】【直播音频】&【直播视频】用户id/ip/设备名单检测器生效（2/2）-直播视频相关
    (cherry picked from commit 76ff9d117c2bfd2a4fb823c69f4d8c7fe2066fd6)

--- yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/video/callback/v4/response/ImageV5SubLabelResp.java
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/video/callback/v4/response/ImageV5SubLabelResp.java
@@ -1,6 +1,5 @@
 package com.netease.yidun.sdk.antispam.video.callback.v4.response;
 
-import com.netease.yidun.sdk.antispam.image.v5.check.sync.response.ImageV5SubLabelDetail;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -86,11 +85,25 @@ public class ImageV5SubLabelResp {
     @AllArgsConstructor
     public static class AntispamInfo {
         /**
+         * 命中的名单类型
+         * 1:自定义用户名单，2:自定义IP名单，3:自定义设备名单
+         */
+        private Integer type;
+        /**
+         * 名单释放时间
+         * -1为永久有效
+         */
+        private Long releaseTime;
+        /**
+         * 策略组名称
+         */
+        private String strategyGroupName;
+        /**
          * 自定义敏感词
          */
         private String word;
         /**
-         * 自定义图片名单url
+         * 自定义图片名单url（命中名单类型的内容）
          */
         private String entity;
         /**
