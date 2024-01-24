670c29dbe40e4237470c7be1c59101dc69abfc7f
关体华 <guantihua@corp.netease.com>
2024-01-24 11:28:49 +0800

    OMSEC-99060 【后端】更新java端sdk，支持返回ipInfo字段中返回sdkIp信息
    summary:
            检测接口返回增加sdkIp字段
    
            版本升级至1.3.3-SNAPSHOT
    
    (cherry picked from commit c1e23553eb0fa1570ff8e4b57d9c5932e1484dcc)

--- yidun-java-sdk/src/main/java/com/netease/yidun/sdk/irisk/v6/check/v603/ip/IpInfo.java
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/irisk/v6/check/v603/ip/IpInfo.java
@@ -15,12 +15,25 @@ import com.netease.yidun.sdk.irisk.v6.check.abst.ip.AbstractIpRiskInfo;
 public class IpInfo<B extends AbstractIpBasicInfo,R extends AbstractIpRiskInfo>  extends AbstractIpInfo implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 易盾通过SDK上报数据链路获取的客户端本地IP，与定位信息可能不同
+     */
+    private String sdkIp;
+
     private B basicInfo;
 
     private R ipRiskInfo;
 
     public B getBasicInfo() {
         return basicInfo;
+    }
+
+    public String getSdkIp() {
+        return sdkIp;
+    }
+
+    public void setSdkIp(String sdkIp) {
+        this.sdkIp = sdkIp;
     }
 
     public void setBasicInfo(B basicInfo) {
@@ -38,6 +51,7 @@ public class IpInfo<B extends AbstractIpBasicInfo,R extends AbstractIpRiskInfo> 
     @Override
     public String toString() {
         return "IpInfo{" +
+                "sdkIp=" + sdkIp +
                 "basicInfo=" + basicInfo +
                 ", ipRiskInfo=" + ipRiskInfo +
                 '}';
