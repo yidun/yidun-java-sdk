a611743107d6162ff70968318ba63ed893ba9370
关体华 <guantihua@corp.netease.com>
2024-03-21 19:30:07 +0800

    OMSEC-102835 【后端】【智能风控】check返回结果中hitInfo新增rules字段
    summay:
            风控检测接口603版本开始支持返回rules字段
            SDK版本升级至1.3.14-SNAPSHOT
    
    (cherry picked from commit 034be62f7f6d9f4793c5989e66aecbc2392087f4)

--- yidun-java-sdk/src/main/java/com/netease/yidun/sdk/irisk/v6/check/v602/HitInfoV602.java
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/irisk/v6/check/v602/HitInfoV602.java
@@ -2,11 +2,18 @@ package com.netease.yidun.sdk.irisk.v6.check.v602;
 
 import com.netease.yidun.sdk.irisk.v6.check.v600.hitinfo.HitInfo;
 
+import java.util.List;
+
 /**
  * 命中标签信息
  */
 public class HitInfoV602 extends HitInfo {
     private Integer riskLevel;
+
+    /**
+     * 603小版本开始支持
+     */
+    private List<String> rules;
 
     public Integer getRiskLevel() {
         return riskLevel;
@@ -16,11 +23,20 @@ public class HitInfoV602 extends HitInfo {
         this.riskLevel = riskLevel;
     }
 
+    public List<String> getRules() {
+        return rules;
+    }
+
+    public void setRules(List<String> rules) {
+        this.rules = rules;
+    }
+
     @Override
     public String toString() {
         return "HitInfoV602{" +
                 "super=" + super.toString() +
-                "riskLevel=" + riskLevel +
+                ", riskLevel=" + riskLevel +
+                ", rules=" + rules +
                 '}';
     }
 }
