bfa801ede3d721ced76cf3768c95c3d865f27488
祝力炀 <zhuliyang02@corp.netease.com>
2024-04-15 13:40:36 +0800

    OMSEC-105474 【后端】【文本】Java/Go sdk中的文本和图片业务支持query接口
    (cherry picked from commit 8f584f0a263d6b1197a1fa0051fc923336d04633)

---
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/text/query/TextTaskIdsQueryResponse.java
@@ -0,0 +1,68 @@
+/*
+ * @(#) TextTaskIdsQueryResponse.java 2024-04-15
+ *
+ * Copyright 2024 NetEase.com, Inc. All rights reserved.
+ */
+
+package com.netease.yidun.sdk.antispam.text.query;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.netease.yidun.sdk.core.response.CommonResponse;
+import com.netease.yidun.sdk.core.utils.StringUtils;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhuliyang02
+ * @version 2024-04-15
+ */
+@Data
+public class TextTaskIdsQueryResponse extends CommonResponse {
+
+    private List<TextTaskIdsQueryResult> result;
+
+    @Data
+    public static class TextTaskIdsQueryResult {
+        private String callback;
+        private int action;
+        private String taskId;
+        private int status;
+        private List<LabelInfo> labels = new ArrayList<>();
+        private Integer censorSource;
+        private Long censorTime;
+        private String censor;
+    }
+
+    @Data
+    public static class LabelInfo {
+        private int label;
+        private int level;
+        private String subLabel;
+        private HintInfo details;
+        public LabelInfo() {
+        }
+        public LabelInfo(int spamType, String hint, int action, String subLabel) {
+            this.label = spamType;
+            this.subLabel = subLabel;
+            details = new HintInfo(hint);
+            this.level = action;
+        }
+    }
+
+    @Data
+    public static class HintInfo {
+        private List<String> hint;
+        public HintInfo() {
+        }
+        public HintInfo(String hint) {
+            if (StringUtils.isNotBlank(hint)) {
+                this.hint = new Gson().fromJson(hint, new TypeToken<List<String>>() {}.getType());
+            } else {
+                this.hint = new ArrayList<>();
+            }
+        }
+    }
+}
