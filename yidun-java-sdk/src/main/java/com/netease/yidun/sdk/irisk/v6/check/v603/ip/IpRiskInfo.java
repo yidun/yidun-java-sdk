c80ae5ea4e7d9024d26e629ced3b94a046a679ad
关体华 <guantihua@corp.netease.com>
2025-03-14 15:17:10 +0800

    OMSEC-131215 【智能风控】修订画像服务风险等级枚举 - 移除智能风控检测接口画像类字段注释，具体字段描述以官网文档为准

--- yidun-java-sdk/src/main/java/com/netease/yidun/sdk/irisk/v6/check/v603/ip/IpRiskInfo.java
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/irisk/v6/check/v603/ip/IpRiskInfo.java
@@ -14,22 +14,10 @@ public class IpRiskInfo extends AbstractIpRiskInfo implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    /**
-     * 风险类型：
-     * 14001-恶意引流、14002-网络诈骗、14003-虚假注册、14004-欺诈团伙、14005-风险设备关联、14006-多重认证、14007-认证高频、14008-多次认证失败、14009-电商欺诈、14010-电信涉诈拦截卡、14011-其他、14012-虚假注册
-     * （多种风险类型以","分割）
-     */
     private String riskType;
 
-    /**
-     * 风险等级：
-     * 0-低风险，1-中风险，2-高风险
-     */
     private Integer riskLevel;
 
-    /**
-     * 风险分值：0.0~1.0，分值越高被黑产持有的概率越高
-     */
     private Double riskScore;
 
     public String getRiskType() {
