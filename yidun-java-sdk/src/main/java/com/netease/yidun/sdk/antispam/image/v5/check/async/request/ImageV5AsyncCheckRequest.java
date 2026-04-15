9e07d4acb48d413c740ee57716800f6bfaab2d3f
hzhumin1 <hzhumin1@corp.netease.com>
2026-04-15 13:51:43 +0800

    OMSEC-158306 【SF】【只读】【图片检测】图片异步检测支持base64

--- yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/image/v5/check/async/request/ImageV5AsyncCheckRequest.java
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/image/v5/check/async/request/ImageV5AsyncCheckRequest.java
@@ -2,6 +2,10 @@ package com.netease.yidun.sdk.antispam.image.v5.check.async.request;
 
 import com.netease.yidun.sdk.antispam.image.v5.check.ImageV5CheckRequest;
 import com.netease.yidun.sdk.antispam.image.v5.check.async.response.ImageV5AsyncCheckResp;
+import com.netease.yidun.sdk.antispam.image.v5.enums.ImageTypeEnum;
+import com.netease.yidun.sdk.core.utils.CollectionUtils;
+
+import java.util.Optional;
 
 public class ImageV5AsyncCheckRequest extends ImageV5CheckRequest<ImageV5AsyncCheckResp> {
 
@@ -9,6 +13,21 @@ public class ImageV5AsyncCheckRequest extends ImageV5CheckRequest<ImageV5AsyncCh
 
     @Override
     protected String requestUriPath() {
+        return "/v5/image/asyncCheck";
+    }
+
+    @Override
+    public String uriPattern() {
+        // 如果有base64图片，调用base64Check接口
+        if (!CollectionUtils.isEmpty(this.getImages())) {
+            boolean hasBase64 = this.getImages().stream()
+                    .anyMatch(imageBeanRequest -> ImageTypeEnum.BASE64.getType() == Optional
+                            .ofNullable(imageBeanRequest.getType())
+                            .orElse(0));
+            if (hasBase64) {
+                return "/v5/image/asyncBase64Check";
+            }
+        }
         return "/v5/image/asyncCheck";
     }
 
