c1e748245d58dc0d5bae457f6a883967d7984248
piaohongren <piaohongren@corp.netease.com>
2026-07-01 10:32:00 +0800

    OMSEC-164242 【SF】【只读】【大小模型融合检测】直播视频接入大小融合检测

--- yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/livevideosolution/callback/v3/response/LiveVideoSolutionLlmCheckCallbackRespV4.java
+++ yidun-java-sdk/src/main/java/com/netease/yidun/sdk/antispam/livevideosolution/callback/v3/response/LiveVideoSolutionLlmCheckCallbackRespV4.java
@@ -18,6 +18,10 @@ public class LiveVideoSolutionLlmCheckCallbackRespV4 implements BaseResponse {
      * 音频片段列表
      */
     private LlmCheckAudioSegment audio;
+    /**
+     * 视频截图检测结果
+     */
+    private LlmCheckVideoPicture video;
 
     @Data
     @AllArgsConstructor
@@ -37,6 +41,36 @@ public class LiveVideoSolutionLlmCheckCallbackRespV4 implements BaseResponse {
         private Long startTime;
         /**
          * 片段结束时间（毫秒时间戳）
+         */
+        private Long endTime;
+        /**
+         * 回调
+         */
+        private String callback;
+        /**
+         * 详情列表
+         */
+        private List<LlmCheckDetail> details;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LlmCheckVideoPicture implements BaseResponse {
+        /**
+         * 截图ID
+         */
+        private String pictureId;
+        /**
+         * 说话人ID
+         */
+        private String speakerId;
+        /**
+         * 截图开始时间
+         */
+        private Long startTime;
+        /**
+         * 截图结束时间
          */
         private Long endTime;
         /**
